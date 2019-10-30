470de8c1a7072374782246f5d12ef0aff15fff88
Geoffrey Miller <gmiller@vailsys.com>
2019-10-30 12:02:58 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using com.freeclimb.api;
+using com.freeclimb.api.queue;
+
+namespace GetQueue {
+  class Program {
+
+    static string getFreeClimbAccountId () { return System.Environment.GetEnvironmentVariable("ACCOUNT_ID"); }
+    static string getFreeClimbAccountToken () {return System.Environment.GetEnvironmentVariable("AUTH_TOKEN"); }
+    static void Main (string[] args) {
+      string queueId = "";
+      // Create FreeClimbClient object
+      FreeClimbClient client = new FreeClimbClient (getFreeClimbAccountId (),
+        getFreeClimbAccountToken ());
+
+      // Invoke get method to retrieve queued metadata
+      Queue queue = client.getQueuesRequester.get (queueId);
+      Console.WriteLine(queue.getQueueId);
+    }
+  }
+}
